ac14e46b60e2f8f89cc06e88986255f60760f3d3
adityasahane076 <adityasahane176@gmail.com>
2025-06-21 02:44:27 +0530

    Add files via upload

---
+++ Assignment31/asg31.1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef unsigned int UNIT;
+
+UNIT OffBit(UNIT iNo)
+{
+    UNIT iResult = 0;
+    UNIT iMask = 0xFFFFFFBF;
+
+    iResult = iMask & iNo;
+
+    return iResult;
+}
+
+int main()
+{
+    UNIT iValue = 0;
+    UNIT iRet = 0;
+
+    printf("Enter the Number\n");
+    scanf("%d",&iValue);
+
+    iRet = OffBit(iValue);
+
+    printf("The updated Number is %d",iRet);
+    return 0;
+}
---
+++ Assignment31/asg31.4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef unsigned int UNIT;
+
+UNIT ToggleBit(UNIT iNo)
+{
+    UNIT iResult = 0;
+    UNIT iMask = 0x240;
+
+    iResult = iMask ^ iNo;
+
+    return iResult;
+}
+
+int main()
+{
+    UNIT iValue = 0;
+    UNIT iRet = 0;
+
+    printf("Enter the Number\n");
+    scanf("%d",&iValue);
+
+    iRet = ToggleBit(iValue);
+
+    printf("The updated Number is %d",iRet);
+    return 0;
+}
